e49da3b1af9a6f3b34fa683572c1f3c65e8c303c
phenix1229 <54951732+phenix1229@users.noreply.github.com>
2025-04-25 17:39:23 -0400

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,21 +18,10 @@ async function bootstrap() {
   const port = configService.get<number>('PORT');
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  // app.enableCors();
-  // app.enableCors({
-  //   allowedHeaders:['Access-Control-Allow-Origin','http://localhost:5173/'],
-  //   exposedHeaders:['Access-Control-Allow-Origin'],
-  //   origin:'http://localhost:5173/',
-  //   // origin:'*',
-  //   methods: 'GET, POST, PATCH, PUT, HEAD',
-  //   credentials:true,
-  // });
-  app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  app.enableCors({ 
+  origin:'http://localhost:5173/',
+  methods: 'GET, POST, PATCH, PUT, HEAD',
+  credentials:true,
   });
   await app.listen(port);
 }
